060ce9b421f708be8a823b1bac99e3d4f979f82b
Jahmali Mcknight <jahmali@rampinteractive.onmicrosoft.com>
2024-11-14 19:58:32 -0700

    added unit test that all passed

---
+++ PacificProgramming.Tests/UnitTests/AvatarImageServiceUnitTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MockQueryable.Moq;
+using Moq;
+using PacificProgramming.Application.Factories;
+using PacificProgramming.Application.Services;
+using PacificProgramming.Application.Strategies;
+using PacificProgramming.Application.ViewModels;
+using PacificProgramming.Domain.PptEntities;
+using PacificProgramming.Infrastructure.Interfaces;
+
+namespace PacificProgramming.Tests.UnitTests;
+
+public sealed class AvatarImageServiceUnitTests {
+    private readonly Mock<IImageService> _imageServiceMock;
+    private readonly Mock<IAvatarImageStrategyFactory> _avatarImageStrategyFactoryMock;
+    private readonly Mock<IPptDbContext> _pptDbContextMock;
+    private readonly AvatarImageService _avatarImageService;
+
+    public AvatarImageServiceUnitTests() {
+        _imageServiceMock = new Mock<IImageService>();
+        _avatarImageStrategyFactoryMock = new Mock<IAvatarImageStrategyFactory>();
+        _pptDbContextMock = new Mock<IPptDbContext>();
+
+        _avatarImageService = new AvatarImageService(_avatarImageStrategyFactoryMock.Object);
+    }
+
+    [Theory]
+    [InlineData("", "https://api.dicebear.com/8.x/pixel-art/png?seed=default&size=150")]
+    public async Task GetAvatarImage_WithEmptyIdentifier_ReturnsImageWithDefaultUrl(string userIdentifier, string expectedUrl) {
+        // Arrange
+        _avatarImageStrategyFactoryMock.Setup(s => s.GetStrategy(userIdentifier)).Returns(new DefaultAvatarImageStrategy());
+
+        // Act
+        var result = await _avatarImageService.GetAvatarImage(userIdentifier);
+
+        // Assert
+        Assert.Equal(result.Id, 0);
+        Assert.Equal(result.Url, expectedUrl);
+    }
+
+    [Theory]
+    [InlineData("myth1", 1, "https://api.dicebear.com/8.x/pixel-art/png?seed=db1&size=150")]
+    public async Task GetAvatarImage_WithLastDigitBetweenOneAndFiveInIdentifier_ReturnsImageWithUrlFromDatabase(string userIdentifier, int expectedId, string expectedUrl) {
+        // Arrange
+        List<Image> images = new List<Image> { new Image { Id = expectedId, Url = "https://api.dicebear.com/8.x/pixel-art/png?seed=db1&size=150" } };
+        Mock<DbSet<Image>> dbSetImageMock = images.AsQueryable().BuildMockDbSet();
+        _avatarImageStrategyFactoryMock.Setup(s => s.GetStrategy(userIdentifier)).Returns(new DatabaseAvatarImageStrategy(_imageServiceMock.Object));
+        _imageServiceMock.Setup(s => s.GetImageById(expectedId)).ReturnsAsync(images.Select(x => new ImageVM { Id = x.Id, Url = x.Url }).FirstOrDefault());
+        _pptDbContextMock.Setup(s => s.Images).Returns(dbSetImageMock.Object);
+
+        // Act
+        var result = await _avatarImageService.GetAvatarImage(userIdentifier);
+
+        // Assert
+        Assert.Equal(result.Id, expectedId);
+        Assert.Equal(result.Url, expectedUrl);
+    }
+
+    [Theory]
+    [InlineData("myth7", "https://api.dicebear.com/8.x/pixel-art/png?seed=7&size=150")]
+    public async Task GetAvatarImage_WithLastDigitBetweenSixAndNineInIdentifier_ReturnsImageWithUrlFromJson(string userIdentifier, string expectedUrl) {
+        // Arrange
+        _avatarImageStrategyFactoryMock.Setup(s => s.GetStrategy(userIdentifier)).Returns(new JsonAvatarImageStrategy(It.IsAny<CancellationToken>()));
+
+        // Act
+        var result = await _avatarImageService.GetAvatarImage(userIdentifier);
+
+        // Assert
+        Assert.Equal(result.Id, 7);
+        Assert.Equal(result.Url, expectedUrl);
+    }
+
+    [Theory]
+    [InlineData("mythos", "https://api.dicebear.com/8.x/pixel-art/png?seed=vowel&size=150")]
+    public async Task GetAvatarImage_WithVowelInIdentifier_ReturnsImageWithVowelUrl(string userIdentifier, string expectedUrl) {
+        // Arrange
+        _avatarImageStrategyFactoryMock.Setup(s => s.GetStrategy(userIdentifier)).Returns(new VowelAvatarImageStrategy());
+
+        // Act
+        var result = await _avatarImageService.GetAvatarImage(userIdentifier);
+
+        // Assert
+        Assert.Equal(result.Id, 0);
+        Assert.Equal(result.Url, expectedUrl);
+    }
+
+    [Theory]
+    [InlineData("myth+", "https://api.dicebear.com/8.x/pixel-art")]
+    public async Task GetAvatarImage_WithSpecialCharacterInIdentifier_ReturnsImageWithRandomUrl(string userIdentifier, string expectedUrl) {
+        // Arrange
+        _avatarImageStrategyFactoryMock.Setup(s => s.GetStrategy(userIdentifier)).Returns(new RandomAvatarImageStrategy());
+
+        // Act
+        var result = await _avatarImageService.GetAvatarImage(userIdentifier);
+
+        // Assert
+        Assert.InRange<int>(result.Id, 1, 5);
+        Assert.StartsWith(expectedUrl, result.Url);
+    }
+
+    [Theory]
+    [InlineData("myth", "https://api.dicebear.com/8.x/pixel-art/png?seed=default&size=150")]
+    public async Task GetAvatarImage_WithNoMetConditionsIdentifier_ReturnsImageWithDefaultUrl(string userIdentifier, string expectedUrl) {
+        // Arrange
+        _avatarImageStrategyFactoryMock.Setup(s => s.GetStrategy(userIdentifier)).Returns(new DefaultAvatarImageStrategy());
+
+        // Act
+        var result = await _avatarImageService.GetAvatarImage(userIdentifier);
+
+        // Assert
+        Assert.Equal(result.Id, 0);
+        Assert.Equal(result.Url, expectedUrl);
+    }
+}
---
+++ PacificProgramming.Tests/UnitTests/AvatarImageStrategyFactoryUnitTests.cs
@@ -0,0 +1,89 @@
+﻿using Moq;
+using PacificProgramming.Application.Factories;
+using PacificProgramming.Application.Services;
+using PacificProgramming.Application.Strategies;
+
+namespace PacificProgramming.Tests.UnitTests;
+public sealed class AvatarImageStrategyFactoryUnitTests {
+    private readonly Mock<IServiceProvider> _serviceProviderMock;
+    private readonly Mock<IImageService> _imageServiceMock;
+    private readonly IAvatarImageStrategyFactory _avatarImageStrategyFactory;
+
+    public AvatarImageStrategyFactoryUnitTests() {
+        _serviceProviderMock = new Mock<IServiceProvider>();
+        _imageServiceMock = new Mock<IImageService>();
+        _avatarImageStrategyFactory = new AvatarImageStrategyFactory(_serviceProviderMock.Object);
+    }
+
+    [Fact]
+    public void GetStrategy_WithEmptyIdentifier_ReturnsDefaultAvatarImageStrategy() {
+        // Arrange
+        _serviceProviderMock.Setup(s => s.GetService(typeof(DefaultAvatarImageStrategy))).Returns(new DefaultAvatarImageStrategy());
+
+        // Act
+        var result = _avatarImageStrategyFactory.GetStrategy("");
+
+        // Assert
+        Assert.IsType<DefaultAvatarImageStrategy>(result);
+    }
+
+    [Fact]
+    public void GetStrategy_WithLastDigitBetweenOneAndFiveInIdentifier_ReturnsDatabaseAvatarImageStrategy() {
+        // Arrange
+        _serviceProviderMock.Setup(s => s.GetService(typeof(DatabaseAvatarImageStrategy))).Returns(new DatabaseAvatarImageStrategy(_imageServiceMock.Object));
+
+        // Act
+        var result = _avatarImageStrategyFactory.GetStrategy("myth1");
+
+        // Assert
+        Assert.IsType<DatabaseAvatarImageStrategy>(result);
+    }
+
+    [Fact]
+    public void GetStrategy_WithLastDigitBetweenSixAndNineInIdentifier_ReturnsJsonAvatarImageStrategy() {
+        // Arrange
+        _serviceProviderMock.Setup(s => s.GetService(typeof(JsonAvatarImageStrategy))).Returns(new JsonAvatarImageStrategy(It.IsAny<CancellationToken>()));
+
+        // Act
+        var result = _avatarImageStrategyFactory.GetStrategy("myth7");
+
+        // Assert
+        Assert.IsType<JsonAvatarImageStrategy>(result);
+    }
+
+    [Fact]
+    public void GetStrategy_WithVowelInIdentifier_ReturnsVowelAvatarImageStrategy() {
+        // Arrange
+        _serviceProviderMock.Setup(s => s.GetService(typeof(VowelAvatarImageStrategy))).Returns(new VowelAvatarImageStrategy());
+
+        // Act
+        var result = _avatarImageStrategyFactory.GetStrategy("mythos");
+
+        // Assert
+        Assert.IsType<VowelAvatarImageStrategy>(result);
+    }
+
+    [Fact]
+    public void GetStrategy_WithSpecialCharacterInIdentifier_ReturnsRandomAvatarImageStrategy() {
+        // Arrange
+        _serviceProviderMock.Setup(s => s.GetService(typeof(RandomAvatarImageStrategy))).Returns(new RandomAvatarImageStrategy());
+
+        // Act
+        var result = _avatarImageStrategyFactory.GetStrategy("myth+");
+
+        // Assert
+        Assert.IsType<RandomAvatarImageStrategy>(result);
+    }
+
+    [Fact]
+    public void GetStrategy_WithNoMetConditionsIdentifier_ReturnsDefaultAvatarImageStrategy() {
+        // Arrange
+        _serviceProviderMock.Setup(s => s.GetService(typeof(DefaultAvatarImageStrategy))).Returns(new DefaultAvatarImageStrategy());
+
+        // Act
+        var result = _avatarImageStrategyFactory.GetStrategy("myth");
+
+        // Assert
+        Assert.IsType<DefaultAvatarImageStrategy>(result);
+    }
+}
